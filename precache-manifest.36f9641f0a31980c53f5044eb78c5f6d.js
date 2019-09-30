7e8504a26a3e9fcdd0db43ab775017e3a4695211
Gaurav soni <Gaurav@allcadservices.com>
2019-09-30 16:26:46 +0400

    Updates

---
+++ precache-manifest.36f9641f0a31980c53f5044eb78c5f6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e978367d04083ea9b91a8be1e5e70d",
+    "url": "/gauravsoni1/eshop/index.html"
+  },
+  {
+    "revision": "a9743fb584f995eeebef",
+    "url": "/gauravsoni1/eshop/static/css/main.dd828123.chunk.css"
+  },
+  {
+    "revision": "4ab031a87cf5af0e7106",
+    "url": "/gauravsoni1/eshop/static/js/2.2d8f1c69.chunk.js"
+  },
+  {
+    "revision": "a9743fb584f995eeebef",
+    "url": "/gauravsoni1/eshop/static/js/main.88c08304.chunk.js"
+  },
+  {
+    "revision": "82eaa455c07cb930b431",
+    "url": "/gauravsoni1/eshop/static/js/runtime-main.a0fe6267.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gauravsoni1/eshop/precache-manifest.36f9641f0a31980c53f5044eb78c5f6d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gauravsoni1/eshop/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
